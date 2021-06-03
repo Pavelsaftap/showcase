9c16c6c5cafe70022ff538e2f35ef694b47acec0
Vasek <kenny.zk46@gmail.com>
2021-06-03 18:15:06 +0300

    adding registration and authorization

--- showcase/main/urls.py
+++ showcase/main/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.main, name='index'),
     path('authorization', views.authorization, name='authorization'),
     path('search', views.search, name='search'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
 
 
 ]
