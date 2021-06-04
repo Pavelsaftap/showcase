178e014f37dd697e2641bd0a7248a5521e8fee63
saftap <pavel541025@yandex.ru>
2021-06-04 11:15:14 +0300

    Project page

--- showcase/main/urls.py
+++ showcase/main/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='index'),
+    path('', views.main, name='main'),
     path('authorization', views.authorization, name='authorization'),
     path('search', views.search, name='search'),
+    path('project', views.project_page, name='project'),
 
 
 ]
--- showcase/main/views.py
+++ showcase/main/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import CityModel, TypeModel, MarkModel, ProjectModel
 
 
 def main(request):
-    a = ProjectModel.objects.all()
-    return render(request, "main/main.html", {'content': 'Проектики', 'title': 'main', 'projects': a})
+    access_level = 1
+    projects = ProjectModel.objects.all()
+    projects_list = []
+    for project in projects:
+        if (project.AccessLevel<=access_level):
+            projects_list.append(project)
+
+    return render(request, "main/main.html", {'content': 'Проектики', 'title': 'main', 'projects': projects_list})
 
 
 def authorization(request):
@@ -15,4 +20,13 @@ def authorization(request):
 def search(request):
     return render(request, "search/search.html")
 
+
+def project_page(request):
+    project = ProjectModel.objects.get(id=2)
+    access_level = 0
+
+    if (project.AccessLevel>access_level):
+        return render(request, "default.html", {'content': 'Нет доступа', 'title': 'Project'})
+    else:
+        return render(request, "project/project.html", {'content': 'Проектик', 'title': 'Project', 'project': project})
 
