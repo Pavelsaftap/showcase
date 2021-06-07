c4d471f51762ae3d9f17c90270904198514a89f1
saftap <pavel541025@yandex.ru>
2021-06-07 19:05:15 +0300

    search, sort added

--- showcase/main/urls.py
+++ showcase/main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='index'),
     path('search', views.search, name='search'),
+    path('search/params=<str:params>', views.search, name='search_params'),
     path('project/id=<int:project_id>', views.project_page, name='project'),
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.logout_user, name='logout'),
--- showcase/main/views.py
+++ showcase/main/views.py
@@ -8,15 +8,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from random import shuffle
 
 
-def main(request):
+def check_access(request, projects, check_main = 0):
     access_level = 1
-    projects = ProjectModel.objects.all()
-
     projects_list = []
+
     for project in projects:
-        if (project.AccessLevel>access_level or project.ShowMainPage==0):
+        if (project.AccessLevel>access_level or (project.ShowMainPage==0 and check_main == 1)):
             continue
 
         sum = 0
@@ -29,18 +29,75 @@ def main(request):
 
         projects_list.append(project)
 
+    return projects_list
+
+
+
+
+def main(request):
+    projects = ProjectModel.objects.all()
+    projects_list = check_access(request, projects, 1)
+
     return render(request, "main/main.html", {'content': 'Проектики', 'title': 'main', 'projects': projects_list})
 
 
-def search(request):
-    return render(request, "search/search.html")
+def parse_search(params):
+    params += ';'
+    params_parse_list = params.split(';')
+    sortby = params_parse_list[0]
+    reverse = 0
+    try:
+        if sortby[-1] == '$':
+            sortby = sortby[:-1]
+            reverse = 1
+        sortby = int(sortby)
+    except:
+        sortby = 0
+    return [[sortby, reverse]]
+
+
+def get_sort(projects_list, search_ask):
+    if search_ask[0][0] == 0:
+        shuffle(projects_list)
+    elif search_ask[0][0] == 1:
+        projects_list.sort(key=lambda x: x.AverageMark, reverse = True)
+    elif search_ask[0][0] == 2:
+        projects_list.sort(key=lambda x: x.Name)
+    elif search_ask[0][0] == 3:
+        projects_list.sort(key=lambda x: x.AccessLevel, reverse = True)
+
+    if search_ask[0][1] == 1:
+        projects_list.reverse()
+    return projects_list
+
+def search(request, params=''):
+
+    sort_list = [{'link':'http://127.0.0.1:8000/search/params=0', 'val':'Random'},
+                {'link':'http://127.0.0.1:8000/search/params=1', 'val':'AvgMark'},
+                {'link':'http://127.0.0.1:8000/search/params=2', 'val':'Name'},
+                {'link':'http://127.0.0.1:8000/search/params=3', 'val':'AccessLevel'}]
+
+    if (params.count(';') > 1 or params == ''):
+        projects = ProjectModel.objects.all()
+        projects_list = check_access(request, projects, 1)
+        return render(request, "search/search.html", {'content': params, 'title': 'search', 'sort_list': sort_list, 'projects': projects_list})
+
+    search_ask = parse_search(params)
+
+    projects = ProjectModel.objects.all()
+    projects_list = check_access(request, projects)
+    projects_list = get_sort(projects_list, search_ask)
+
+
+    return render(request, "search/search.html", {'content': params, 'title': 'search', 'sort_list': sort_list, 'projects': projects_list})
 
 
 def project_page(request, project_id):
     result = 0
     try:
         project = ProjectModel.objects.get(id=project_id)
-        access_level = 0
+
+        access_level = 1
         if (project.AccessLevel > access_level):
             result = 1
         sum = 0
